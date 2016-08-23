c2bb49b9416a57cc08dee8c7d177a781d353c372
Fily <fily.junior@gmail.com>
2016-08-23 00:01:07 -0400

    SQLite Windows Phone

---
+++ TaxDedutions/TaxDedutions.WinPhone/Win_SQLite.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+using Xamarin.Forms;
+using Windows.Storage;
+using System.IO;
+using TaxDedutions.WinPhone;
+using TaxDedutions.Interfaces;
+
+[assembly: Dependency(typeof(WinSQLite))]
+
+namespace TaxDedutions.WinPhone
+{
+    public class WinSQLite : ISQLite
+    {
+        public SQLiteConnection GetConnection()
+        {
+            var dbName = "Records.db3";
+            var path = Path.Combine(ApplicationData.
+              Current.LocalFolder.Path, dbName);
+            return new SQLiteConnection(path);
+        }
+    }
+}
